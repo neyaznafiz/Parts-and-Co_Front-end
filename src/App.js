1f0c32608769f7ea28ffa7e406fce3dbae782f98
niiyazobug <niiyaz5555@gmail.com>
2022-05-22 02:29:35 +0600

    favicon and title added and 404 page created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home'
 import About from './Pages/About'
 import Login from './Pages/UserAuthentication/Login'
 import Signup from './Pages/UserAuthentication/Signup'
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
 
---
+++ src/Components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <p>404</p>
+            <p>page not found</p>
+        </div>
+    );
+};
+
+export default NotFound;
