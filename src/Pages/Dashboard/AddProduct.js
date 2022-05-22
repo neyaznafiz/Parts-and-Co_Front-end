9f66e1aefb84c94964433e28d29ee30de4687351
niiyazobug <niiyaz5555@gmail.com>
2022-05-23 04:06:07 +0600

    add revice form and add product form added

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const AddProduct = () => {
     return (
-        <div>
-            <h2>this is add product</h2>
+        <div className='lg:w-[500px]'>
+
+            <div className='text-center'>
+                <h2 className='text-3xl font-bold mb-7 '>ADDED YOUR PRODUCT HERE</h2>
+            </div>
+
+            <form>
+
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form4Example1">NAME</label>
+                    <input type="text" name='name' id="form4Example1" className="form-control outline outline-stone-500 border" />
+                </div>
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form4Example1">PRICE</label>
+                    <input type="number" name='price' id="form4Example1" className="form-control outline outline-stone-500 border" />
+                </div>
+                
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form4Example1">QUANTITY</label>
+                    <input type="number" name='quantity' id="form4Example1" className="form-control outline outline-stone-500 border" />
+                </div>
+
+            
+                {/* <!-- Message input --> */}
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form4Example3">DISCRIPTION</label>
+                    <textarea className="form-control outline outline-stone-500 border" id="form4Example3" rows="4"></textarea>
+                </div>
+
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form4Example1">IMAGE</label>
+                    <input type="file" name='image' id="form4Example1" className="form-control outline outline-stone-500 border" />
+                </div>
+
+                <input type="submit" value='ADD PRODUCT' className="btn btn-outline w-full px-3 py-1 rounded-md hover:bg-transparent hover:text-black" />
+            </form>
+
         </div>
     );
 };
--- src/Pages/Dashboard/AddaReview.js
+++ src/Pages/Dashboard/AddaReview.js
@@ -1,23 +1,57 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddaReview = () => {
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+
+    const onSubmit = async (data) => { };
+
     return (
         <div>
-            <div className='mb-5 text-center text-2xl font-bold'>
-            <h2>WELCOME TO REVIEW SECTION</h2>
-            </div>
-            <div class="form-control bg-transparent border-0 lg:w-[500px]">
-                <label class="label">
+            <div className="card lg:w-[500px] lg:h-[400px] bg-base-100 shadow-xl mx-auto items-center mb-20 bg-transparent">
+                <div className="card-body w-full">
+                    <h2 className="text-center text-2xl font-bold pb-7">WELCOME TO REVIEW SECTION</h2>
+                    <div>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="form-control  max-w-xs bg-transparent border-0">
+                                <label className="label">
+                                    <span className="label-text">TYPE YOUR REVIEW</span>
+                                </label>
+                                <textarea type="text" placeholder="yourReview" name="yourReview" className="input input-bordered lg:w-[410px] h-[] h-[100px] bg-transparent"
+                                    {...register("yourReview", {
+                                        required: {
+                                            value: true,
+                                            message: "yourReview is required",
+                                        },
+                                        minLength: {
+                                            value: 40,
+                                            message: "your yourReview  must be 40 character",
+                                        },
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.yourReview?.type === "required" && (
+                                        <span className="label-text-alt text-red-500">
+                                            {errors.yourReview.message}
+                                        </span>
+                                    )}
+                                    {errors.yourReview?.type === "minLength" && (
+                                        <span className="label-text-alt text-red-500">
+                                            {errors.yourReview.message}
+                                        </span>
+                                    )}
+                                </label>
+                            </div>
 
-                    <span class="label-text text-xl">TYPE YOUR REVIEW</span>
-                    
-                </label>
-                <textarea type='text' name='review' class="textarea textarea-bordered h-24 bg-transparent " ></textarea>
-                <label class="label">
-
-                <input type="submit" value='SUBMIT' className="btn btn-outline w-full px-3 py-1 rounded-md hover:bg-transparent hover:text-black" />
-                    
-                </label>
+                            <input type="submit" value='SUBMIT' className="btn btn-outline w-full px-3 py-1 rounded-md hover:bg-transparent hover:text-black" />
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     );
