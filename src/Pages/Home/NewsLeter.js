0b13a9f74eb44452ec614b8a777e904c6a43bdc8
niiyazobug <niiyaz5555@gmail.com>
2022-05-22 20:59:34 +0600

    livesite link ans server site code link added

--- src/Pages/Home/NewsLeter.js
+++ src/Pages/Home/NewsLeter.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const NewsLeter = () => {
     return (
-        <div>
-            <div className=' relative my-10 py-20 lg:py-40 w-full bg-no-repeat bg-stone-500'>
+        <div className='bg-stone-500'>
+            <div className=' relative my-10 py-20 lg:py-40 w-full bg-no-repeat'>
 
                 <div className='text-center'>
-                    <h2 className='font-serif text-md lg:text-3xl pb-3 text-slate-300 '>Subscribe for get newsleter about new technologies in every month.</h2>
+                    <h2 className='font-serif text-md lg:text-3xl pb-3 text-slate-300 '>Subscribe and get newsleter about new technologies in every month.</h2>
 
                     <div className='grid lg:flex justify-center lg:pr-12 lg:mt-20 opacity-75'>
                         <input className='rounded-full border-2 lg:pr-40 pl-5 py-2 card-shadow' type="email" name="email" id="" placeholder='ENTER YOUR EMAIL' required />
