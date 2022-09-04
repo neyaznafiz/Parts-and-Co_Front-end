a76151b58555c11d8f3a43e215e76b244bf072b8
neyaznafiz <niiyazobug@gmail.com>
2022-09-05 03:20:16 +0600

    responsive issu

--- src/Pages/Home/parts-products/ProductsInHome.js
+++ src/Pages/Home/parts-products/ProductsInHome.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import useProducts from '../../../Hooks/useProducts';
 import DisplayProductsInHome from './DisplayProductsInHome';
 
@@ -11,16 +13,21 @@ const ProductsInHome = () => {
         <div className=''>
 
             <div className='pt-10'>
-                <p className='text-center text-2xl lg:text-4xl uppercase '>get here your needed parts</p>
+                <p className='text-center text-2xl lg:text-4xl uppercase'>get here your needed parts</p>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-20 lg:pb-10 lg:px-10 '>
+            <div className='grid grid-cols-1 lg:grid-cols-2 lg:gap-20 p-3 lg:px-10 '>
                 {
-                    [...products].reverse().slice(0,3).map(product => <DisplayProductsInHome
+                    [...products].reverse().slice(0, 2).map(product => <DisplayProductsInHome
                         key={product._id}
                         product={product}
                     />)
                 }
+            </div>
+
+            <div className='flex justify-end mx-16 pb-4 lg:pb-10'>
+                <div></div>
+                <Link to='/allproducts' className='border-b border-stone-600 pr-3 flex w-[260px] text-center  hover:text-black/60'> <span className='flex gap-2 text-[20px] font-semibold'>SEE ALL PPRODUCTS <AiOutlineArrowRight className='mt-1 ' /> </span> </Link>
             </div>
 
         </div>
--- src/Pages/Products/AllProducts.js
+++ src/Pages/Products/AllProducts.js
@@ -8,7 +8,7 @@ const AllProducts = () => {
    const [products]= useProducts()
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 p-10'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-20 p-3 lg:p-10'>
             {
                 products.map(product => <DisplayAllProducts
                     key={product._id}
